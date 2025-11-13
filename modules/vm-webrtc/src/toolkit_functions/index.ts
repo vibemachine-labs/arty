e49d2a42fe78729490f92a72c79a14deba456d79
Traun Leyden <traun.leyden@gmail.com>
2025-11-13 22:46:34 +0000

    get_file_tree_structure slow and expensive. remove it

--- modules/vm-webrtc/src/toolkit_functions/index.ts
+++ modules/vm-webrtc/src/toolkit_functions/index.ts
@@ -39,7 +39,6 @@ export const toolkitRegistry: ToolkitRegistry = {
     keyword_search: googleDrive.keyword_search,
     search_documents: googleDrive.search_documents,
     search_and_retrieve_documents: googleDrive.search_and_retrieve_documents,
-    get_file_tree_structure: googleDrive.get_file_tree_structure,
   },
 };
 

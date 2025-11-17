bbb743b697bde3910b49060b9146ac3c0a5b2a93
Traun Leyden <traun.leyden@gmail.com>
2025-11-17 18:17:31 +0100

    trigger reload event.  untested

--- lib/toolPrompts.ts
+++ lib/toolPrompts.ts
@@ -1,4 +1,7 @@
+import { DeviceEventEmitter } from "react-native";
+
 import { loadPromptAddition, savePromptAddition } from "./promptStorage";
+import { CONNECTOR_SETTINGS_CHANGED_EVENT } from "../modules/vm-webrtc/src/ToolkitManager";
 
 const TOOL_PROMPT_STORAGE_PREFIX = "@vibemachine/toolPrompt/";
 
@@ -8,8 +11,12 @@ export const getToolPromptStorageKey = (toolName: string): string =>
 export const loadToolPromptAddition = (toolName: string): Promise<string> =>
   loadPromptAddition(getToolPromptStorageKey(toolName));
 
-export const saveToolPromptAddition = (
+export const saveToolPromptAddition = async (
   toolName: string,
   addition: string
-): Promise<void> =>
-  savePromptAddition(getToolPromptStorageKey(toolName), addition);
+): Promise<void> => {
+  await savePromptAddition(getToolPromptStorageKey(toolName), addition);
+
+  // Emit event to reload toolkit definitions cache
+  DeviceEventEmitter.emit(CONNECTOR_SETTINGS_CHANGED_EVENT);
+};
